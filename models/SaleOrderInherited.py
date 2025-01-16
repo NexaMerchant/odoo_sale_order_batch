baaa45e7ebf0f455d7cbd7143ba3e7cc05e4f2eb
odoo <nice.lizhi@gmail.com>
2025-01-16 02:44:54 +0000

    fix add bath

--- models/SaleOrderInherited.py
+++ models/SaleOrderInherited.py
@@ -12,7 +12,51 @@ class SaleOrderInherited(models.Model):
         store=True
     )
 
+    delivery_number = fields.Char(
+        string='Delivery Number',
+        compute='_compute_delivery_number',
+        store=True
+    )
+
+    @api.depends('picking_ids')
+    def _compute_delivery_number(self):
+        for order in self:
+            delivery_numbers = order.picking_ids.filtered(lambda p: p.state != 'cancel').mapped('name')
+            order.delivery_number = ', '.join(delivery_numbers)
+
     @api.depends('partner_shipping_id', 'partner_shipping_id.country_id')
     def _compute_shipping_country(self):
         for order in self:
             order.shipping_country_id = order.partner_shipping_id.country_id
+
+    def action_batch_choose_delivery(self):
+        view_id = self.env.ref('delivery.choose_delivery_carrier_view_form').id
+        return {
+            'name': 'Add a shipping method',
+            'type': 'ir.actions.act_window',
+            'view_mode': 'form',
+            'res_model': 'choose.delivery.carrier',
+            'view_id': view_id,
+            'views': [(view_id, 'form')],
+            'target': 'new',
+            'context': {
+                'default_order_ids': self.ids,
+            }
+        }
+
+    def action_choose_delivery(self):
+        self.ensure_one()
+        view_id = self.env.ref('delivery.choose_delivery_carrier_view_form').id
+        return {
+            'name': 'Add a shipping method',
+            'type': 'ir.actions.act_window',
+            'view_mode': 'form',
+            'res_model': 'choose.delivery.carrier',
+            'view_id': view_id,
+            'views': [(view_id, 'form')],
+            'target': 'new',
+            'context': {
+                'default_order_id': self.id,
+                'default_carrier_id': self.carrier_id.id
+            }
+        }
